513495061ef4f2b0e147101ed3232984029804ad
Komal Singh <149627266+Ksingh0023@users.noreply.github.com>
2026-04-14 17:12:47 +0530

    Implement function to find lucky integer in array
    This is easy solution to find lucky number

---
+++ 1394. Find Lucky Integer in an Array .java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findLucky(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : arr) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        int maxLucky = -1;
+        for (int key : map.keySet()) {
+            if (key == map.get(key)) {
+                maxLucky = Math.max(maxLucky, key);
+            }
+        }
+
+        return maxLucky;
+    }
+}
