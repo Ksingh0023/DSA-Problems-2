cc55536ae649ff8bf7029f439419fb6fe0a49633
Komal Singh <149627266+Ksingh0023@users.noreply.github.com>
2026-05-17 21:37:38 +0530

    Implement Balanced Binary Tree solution

---
+++ Balanced Binary Tree.java
@@ -0,0 +1,23 @@
+
+class Solution {
+    
+    public boolean isBalanced(TreeNode root) {
+        return height(root) != -1;
+    }
+
+    public int height(TreeNode root) {
+        
+        if (root == null) {
+            return 0;
+        }
+
+        int left = height(root.left);
+        int right = height(root.right);
+
+        if (left == -1 || right == -1 || Math.abs(left - right) > 1) {
+            return -1;
+        }
+
+        return Math.max(left, right) + 1;
+    }
+}
